46463ea717d0f7fff6c24a1153594486adcf6b49
Alex Goodell <alexgoodell@gmail.com>
2024-08-02 11:23:45 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -26,7 +26,10 @@ sudo wget https://gist.githubusercontent.com/alexgoodell/2e76a0ae2bfb25dccf63aa0
 
 # sudo usermod -a -G adm,google-sudoers jupyter-agoodell
 sudo tljh-config add-item users.admin agoodell
+sudo tljh-config add-item users.admin jupyter-jason959
 sudo tljh-config add-item users.admin jupyter-agoodell
+sudo tljh-config add-item users.admin jupyter-lchu
+sudo tljh-config add-item users.admin jupyter-darar
 
 # reload
 sudo tljh-config reload
